4b12d65bb2fa023eff9aac23741788f7f3967342
McKenna Noakes <mckenna.noakes@outlook.com>
2024-12-08 00:42:19 -0600

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.js_compressor = :uglifier
-
   config.log_level = :debug
 
 end
