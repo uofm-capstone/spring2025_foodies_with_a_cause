cbff1fe1a3ccd143d33790b87ff64cb95633242f
Shrajanya Acharya <u2reach@gmail.com>
2025-04-01 17:35:02 -0500

    Update email testing to use letter_opener and other local changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,3 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
-// Modify Babel loader to include @hotwired/turbo
-const babelLoader = environment.loaders.get('babel');
-babelLoader.exclude = /node_modules\/(?!@hotwired\/turbo)/;
-
-module.exports = environment;
+module.exports = environment
