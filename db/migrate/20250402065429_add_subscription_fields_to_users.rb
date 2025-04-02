5d2a58e0f167743ef0b795bccf76638fb07f3bfe
Miguel Perez <migeperezjr@gmail.com>
2025-04-02 02:26:10 -0500

    Styled the pricing page, added more cols to user model (plan, plan status, and plan ends at)

---
+++ db/migrate/20250402065429_add_subscription_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddSubscriptionFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :plan, :string
+    add_column :users, :subscription_status, :string
+    add_column :users, :subscription_ends_at, :datetime
+  end
+end
